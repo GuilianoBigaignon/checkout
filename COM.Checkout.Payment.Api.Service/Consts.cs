c928b1e88853e7a8e888f2d8308f3a74c3458923
GuilianoBigaignon <54898100+GuilianoBigaignon@users.noreply.github.com>
2019-09-05 17:10:03 +0400

    Add files via upload

---
+++ COM.Checkout.Payment.Api.Service/Consts.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+
+namespace COM.Checkout.Payment.Api.Service
+{
+    public static partial class Consts
+    {
+        // Stratus update liste DV
+        public const string Delta = "delta";
+    }
+}
---
+++ COM.Checkout.Payment.Api.Service/IPaymentServices.cs
@@ -0,0 +1,28 @@
+﻿using COM.Checkout.Payment.Api.Contract.DTO.MessageRequest;
+using Framework.Database;
+using System.Net.Http;
+
+namespace COM.Checkout.Payment.Api.Service
+{
+    /// <summary>
+    /// Payment module
+    /// </summary>
+    public interface IPaymentServices
+    {
+        /// <summary>
+        /// Ajout d'une nouvelle liste DV
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="listeDVitemRequestDTO"></param>
+        /// <returns></returns>
+        HttpResponseMessage InsertListeDV(IDbContext context,CardDetailsRequestDTO listeDVitemRequestDTO);
+
+        /// <summary>
+        /// Mise à jour d'une liste DV existante
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="listeDVitemRequestDTO"></param>
+        /// <returns></returns>
+        HttpResponseMessage UpdateListeDV(IDbContext context, CardDetailsRequestDTO listeDVitemRequestDTO);
+    }
+}
